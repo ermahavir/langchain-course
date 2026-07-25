e961765f7e854272be3f185c9910e04d19a81ff5
Mahavir Prasad <er.mahavir@gmail.com>
2026-07-25 19:33:15 +0530

    Integrate MCP client functionality with LangChain, enabling interaction with a math server and tool loading for enhanced query processing.

--- main.py
+++ main.py
@@ -2,12 +2,41 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
+# ClientSession is framework which allows a python file to act as an MCP client
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage, SystemMessage
+
 load_dotenv() 
 
-async def main():
-    print("Hello from langchain-course!")
+llm = ChatOpenAI(model=os.getenv("OPENAI_MODEL"), base_url=os.getenv("OPENAI_BASE_URL"), temperature=1)
 
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/mprasad/Desktop/courses/langchain-course/servers/math_server.py"],
+)
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Initialized session")
+            tools = await load_mcp_tools(session) #await session.list_tools()
+            #print(tools)
+
+            agent = create_agent(llm, tools)
+
+            system_message = SystemMessage(content=
+                "You are a helpful assistant that can answer questions with the help of given tools. "
+                "Always use provided tools to answer the questions.")
+
+            result = await agent.ainvoke({"messages": [system_message, HumanMessage(content="What is 50 + 5 * 3?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
-    print(os.getenv("OPENAI_API_KEY"))
     asyncio.run(main())
